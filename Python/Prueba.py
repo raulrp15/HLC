7173dbe5af4876829cb54bfc3aba25e89a03fd1a
raulrp15 <raulromerapavon@gmail.com>
2024-10-10 09:24:38 +0200

    first commit

---
+++ Python/Prueba.py
@@ -0,0 +1,8 @@
+def suma(n, m):
+    return n + m
+
+def resta(n, m):
+    return n - m
+
+def multi(n, m):
+    return n * m
---
+++ Python/prueba_test.py
@@ -0,0 +1,10 @@
+from Prueba import *
+
+def test_suma():
+    assert suma(2, 7) == 9
+
+def test_resta():
+    assert resta(1, 6) == -5
+
+def test_multi():
+    assert multi(2, 7) == 14
