7a84f9973b8805f148deff2eb1798caeb9e81e5d
raulrp15 <raulromerapavon@gmail.com>
2024-10-24 19:41:32 +0200

    Fuerzas Realizado

--- Boletin02/Fuerzas.py
+++ Boletin02/Fuerzas.py
@@ -1,6 +1,41 @@
 def CalculoFuerzas(datoP, datoS):
+    resultado = ""
     dato1 = datoP.split("=")
     dato2 = datoS.split("=")
-    if dato1[0] == "D" and dato2[0] == "T":
-        resultado = "V" + (dato1[1] + dato2[1])
+    n1 = int(dato1[1])
+    n2 = int(dato2[1])
+
+    if dato1[0] == 'D' and dato2[0] == 'T':
+        if n2 != 0:
+            v = int(n1 / n2)
+            resultado = f"V={v}"
+        else:
+            resultado = "No se puede dividir entre 0"
+    elif dato1[0] == 'T' and dato2[0] == 'D':
+        if n1 != 0:
+            v = int(n2 / n1)
+            resultado = f"V={v}"
+        else:
+            resultado = "No se puede dividir entre 0"
+    elif dato1[0] == 'V' and dato2[0] == 'T':
+        d = int(n1 * n2)
+        resultado = f"D={d}"
+    elif dato1[0] == 'T' and dato2[0] == 'V':
+        d = int(n2 * n1)
+        resultado = f"D={d}"
+    elif dato1[0] == 'V' and dato2[0] == 'D':
+        if n1 != 0:
+            t = int(n2 / n1)
+            resultado = f"T={t}"
+        else:
+            resultado = "No se puede dividir entre 0"
+    elif dato1[0] == 'D' and dato2[0] == 'V':
+        if n2 != 0:
+            t = int(n1 / n2)
+            resultado = f"T={t}"
+        else:
+            resultado = "No se puede dividir entre 0"
+    else:
+        return "Datos no válidos"
+        
     return resultado
--- Boletin02/Fuerzas_test.py
+++ Boletin02/Fuerzas_test.py
@@ -11,3 +11,15 @@ def test_Fuerzas3():
 
 def test_Fuerzas4():
     assert CalculoFuerzas("D=10", "T=2") == "V=5"
+
+def test_Fuerzas5():
+    assert CalculoFuerzas("D=32", "T=0") == "No se puede dividir entre 0"
+
+def test_Fuerzas6():
+    assert CalculoFuerzas("D=32", "V=8") == "T=4"
+
+def test_Fuerzas7():
+    assert CalculoFuerzas("D=0", "V=4") == "T=0"
+
+def test_Fuerzas8():
+    assert CalculoFuerzas("V=0", "D=2") == "No se puede dividir entre 0"
