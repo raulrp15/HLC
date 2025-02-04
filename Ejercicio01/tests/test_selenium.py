381481e29457dec5e96a840942fc031190832d9b
raulrp15 <raulromerapavon@gmail.com>
2025-02-04 12:49:43 +0100

    wait

--- Ejercicio01/tests/test_selenium.py
+++ Ejercicio01/tests/test_selenium.py
@@ -1,23 +1,25 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 driver = webdriver.Firefox()
-driver.get("http://localhost:3001")
+url = "http://localhost:3000"
 
-def test_completo(first_link:str, second_link:str):
-    driver.find_element(By.ID, first_link).click()
-    sleep(2)
-    driver.find_element(By.ID, second_link).click()
-    sleep(2)
-    driver.switch_to.window(driver.window_handles[0])
-    sleep(2)
-    driver.find_element(By.ID, "Volver").click()
-    sleep(2)
+def test_busquedas():
+    driver.get("http://localhost:3000")
+    bsq = driver.find_element(By.ID, "Buscadores").get_attribute('href')
+    bsq == "http://localhost:3000/buscadores/"
     driver.quit()
 
-if __name__ == "__main__":
-    test_completo('Redes Sociales', 'Instagram')
+def test_redes():
+    driver.get("http://localhost:3000")
+    red = driver.find_element(By.ID, "Redes Sociales").get_attribute('href')
+    red == "http://localhost:3000/redes-sociales/"
+    driver.quit()
+
+def test_volver():
+    driver.get("http://localhost:3000/buscadores/")
+    driver.find_element(By.ID, "Volver").click()
+    inicio = driver.current_url
+    inicio == "http://localhost:3000/"
+    driver.quit()
