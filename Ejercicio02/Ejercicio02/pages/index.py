ca749a6744e7de78354cb9cfcb94e8e7368252f5
raulrp15 <raulromerapavon@gmail.com>
2025-02-13 10:25:26 +0100

    faltan 2 cositas

--- Ejercicio02/Ejercicio02/pages/index.py
+++ Ejercicio02/Ejercicio02/pages/index.py
@@ -5,16 +5,37 @@ def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
         rx.vstack(
-            rx.heading("Formulario de registro", id="titulo"),
+            rx.heading("Formulario de registro - Mi Web", id="titulo"),
             rx.form(
                 rx.hstack(
                     rx.text("Nombre:", id="lname"),
-                    rx.input(placeholder="Ingrese su nombre", id="iname"),
+                    rx.input(placeholder="Ingrese su nombre", id="iname", type="text"),
                 ),
                 rx.hstack(
                     rx.text("Apellidos:", id="lsurname"),
-                    rx.input(placeholder="Ingrese su apellido", id="isurname"),
+                    rx.input(placeholder="Ingrese su apellido", id="isurname", type="text"),
                 ),
+                rx.radio_group(
+                        [
+                            "Hombre",
+                            "Mujer",
+                        ],
+                        name="radio_choice",
+                        direction="row",
+                    ),
+                rx.hstack(
+                    rx.text("Email:", id="lemail"),
+                    rx.input(placeholder="Ingrese su email", id="iemail", type="text"),
+                ),
+                rx.checkbox(
+                    "Deseo recibir información sobre novedades y ofertas.",
+                    checked=True,
+                    id="icheck",
+                ),
+                rx.checkbox(
+                    "Declaro haber leido y aceptar las condiciones generales del programa y la normativa sobre protección de datos",
+                    id="icheck2",
+                )
             ),
         )
     )
--- Ejercicio02/test/formulario_test.py
+++ Ejercicio02/test/formulario_test.py
@@ -1,19 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
+
 
 url = "http://localhost:3000"
 def test_formulario_1():
+    driver = webdriver.Firefox()
     driver.get(url)
     try:
         title = driver.find_element(By.ID,"titulo").text
     finally:
         driver.quit()
     
-    assert title == "Formulario de registro"
+    assert title == "Formulario de registro - Mi Web"
 
 def test_formulario_2():
+    driver = webdriver.Firefox()
     driver.get(url)
     try:
         label_text = driver.find_element(By.ID,"lname").text
@@ -23,6 +25,7 @@ def test_formulario_2():
     assert label_text == "Nombre:"
 
 def test_formulario_3():
+    driver = webdriver.Firefox()
     driver.get(url)
     try:
         input = driver.find_element(By.ID,"iname").tag_name
@@ -32,6 +35,7 @@ def test_formulario_3():
     assert input == "input"
 
 def test_formulario_4():
+    driver = webdriver.Firefox()
     driver.get(url)
     try:
         label_text = driver.find_element(By.ID,"lsurname").text
@@ -41,6 +45,7 @@ def test_formulario_4():
     assert label_text == "Apellidos:"
 
 def test_formulario_5():
+    driver = webdriver.Firefox()
     driver.get(url)
     try:
         input = driver.find_element(By.ID,"isurname").tag_name
@@ -48,3 +53,46 @@ def test_formulario_5():
         driver.quit()
     
     assert input == "input"
+
+def test_formulario_6():
+    driver = webdriver.Firefox()
+    driver.get(url)
+    try:
+        radio_buttons = driver.find_elements(By.XPATH, "//*[@role='radio']")
+        radio_hombre = radio_buttons[0].get_attribute("value")
+        radio_mujer = radio_buttons[1].get_attribute("value")
+    finally:
+        driver.quit()
+    
+    assert radio_hombre == "Hombre" and radio_mujer == "Mujer"
+
+def test_formulario_7():
+    driver = webdriver.Firefox()
+    driver.get(url)
+    try:
+        email = driver.find_element(By.ID,"iemail").tag_name
+    finally:
+        driver.quit()
+    
+    assert email == "input"
+
+def test_formulario_8():
+    driver = webdriver.Firefox()
+    driver.get(url)
+    try:
+        checkbox = driver.find_element(By.ID,"icheck").get_attribute("type")
+        checkbox_checked = driver.find_element(By.ID,"icheck").get_attribute("aria-checked")
+    finally:
+        driver.quit()
+    
+    assert checkbox == "button" and checkbox_checked == "true"
+
+def test_formulario_9():
+    driver = webdriver.Firefox()
+    driver.get(url)
+    try:
+        checkbox = driver.find_element(By.ID,"icheck2").get_attribute("type")
+    finally:
+        driver.quit()
+    
+    assert checkbox == "button"
